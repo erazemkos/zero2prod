08c5944e8422ec9ced12fd4a9f0b4124d8831e08
erkos <erazem.kos@hotmail.com>
2024-03-23 20:52:16 +0100

    main logic in lib

---
+++ src/lib.rs
@@ -0,0 +1,28 @@
+use actix_web::{web, App, HttpResponse, HttpServer};
+use actix_web::dev::Server;
+use std::net::TcpListener;
+
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+#[derive(serde::Deserialize)]
+pub struct FormData {
+    email: String,
+    name: String,
+}
+// Let's start simple: we always return a 200 OK
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscribe", web::post().to(subscribe))
+    })
+        .listen(listener)?
+        .run();
+    Ok(server)
+}
